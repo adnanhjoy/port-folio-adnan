1e9498db40baf4880a3cae54f0147b480d2ae0d7
engadnanhossain <engr.adnanhossain@gmail.com>
2023-07-12 11:04:40 +0600

    add project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Element } from 'react-scroll';
-import img1 from '../../assets/review/person1.jpg'
 import { TfiWorld } from "react-icons/tfi";
 import { FaGithub } from 'react-icons/fa';
 
@@ -11,57 +10,54 @@ const Projects = () => {
                 <div className='w-4/5 mx-auto py-14'>
                     <h1 className='text-center text-4xl text-white'>Projects</h1>
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-14'>
-                        <div className="relative hover:opacity-100">
-                            <img src={img1} alt="Avatar" className="w-full block h-auto" />
+                        <div className="relative h-96 hover:opacity-100 card-bg hover:bg-bottom ease-in-out duration-[3s]">
                             <div className=" absolute top-0 bottom-0 left-0 right-0 h-full w-full opacity-0 transition-[.5s] ease-in bg-black hover:opacity-75 cursor-pointer">
                                 <div className='translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 absolute flex flex-col'>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <TfiWorld></TfiWorld>
                                         <a>Live Site</a>
                                     </span>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <FaGithub></FaGithub>
                                         <a>Client Site</a>
                                     </span>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <FaGithub></FaGithub>
                                         <a>Server Site</a>
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className="relative hover:opacity-100">
-                            <img src={img1} alt="Avatar" className="w-full block h-auto" />
+                        <div className="relative h-96 hover:opacity-100 card-bg hover:bg-bottom ease-in-out duration-[3s]">
                             <div className=" absolute top-0 bottom-0 left-0 right-0 h-full w-full opacity-0 transition-[.5s] ease-in bg-black hover:opacity-75 cursor-pointer">
                                 <div className='translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 absolute flex flex-col'>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <TfiWorld></TfiWorld>
                                         <a>Live Site</a>
                                     </span>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <FaGithub></FaGithub>
                                         <a>Client Site</a>
                                     </span>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <FaGithub></FaGithub>
                                         <a>Server Site</a>
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className="relative hover:opacity-100">
-                            <img src={img1} alt="Avatar" className="w-full block h-auto" />
+                        <div className="relative h-96 hover:opacity-100 card-bg hover:bg-bottom ease-in-out duration-[3s]">
                             <div className=" absolute top-0 bottom-0 left-0 right-0 h-full w-full opacity-0 transition-[.5s] ease-in bg-black hover:opacity-75 cursor-pointer">
                                 <div className='translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 absolute flex flex-col'>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <TfiWorld></TfiWorld>
                                         <a>Live Site</a>
                                     </span>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <FaGithub></FaGithub>
                                         <a>Client Site</a>
                                     </span>
-                                    <span className='text-white text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
+                                    <span className='text-white md:text-xl flex justify-center items-center gap-2 hover:bg-[#0f172a] py-1 px-3 rounded mb-3 border'>
                                         <FaGithub></FaGithub>
                                         <a>Server Site</a>
                                     </span>
